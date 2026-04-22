658b7d21f8e936538dd255aab0012810b149bf0b
Harshada1999-git <harshada211999neware@gmail.com>
2026-04-22 09:35:51 +0530

    added index.js

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,6 +1,11 @@
 const core = require('@actions/core');
  
 async function run() { 
+  /*
+  1. Parse Inputs:
+    1.1 base-branch from which to check for updates
+    1.2 target
+  */
   core.info('I am a custom JS action');
 }
  
