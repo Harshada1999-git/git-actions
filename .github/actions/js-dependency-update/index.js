79ae1b9560d780031f134679e2782d723b527cf3
Harshada1999-git <harshada211999neware@gmail.com>
2026-04-22 10:30:18 +0530

    79- practical exercise

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,6 +1,50 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
  
+const validateBranchName = ({ branchName }) => /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
+const ValidateDirectoryName = ({dirName}) => a-zA-Z0-9-+$/test(dirName);
+  
 async function run() { 
+  const baseBranch = core.getInput('base-Branch');
+  const targetBranch = core.getInput('target-Branch');
+  const ghToken = core.getInput('ghToken');
+  const workingDir = core.getInput('working-directory');
+  const debug = core.getInput('debug');
+
+  core.setSecret(ghToken);
+
+  if (validateBranchName({branchName: baseBranch})){
+    core.setFailed('invalid base branch name')
+    return;
+  }
+
+  if (validateBranchName({branchName: targetBranch})){
+    core.setFailed('invalid target-branch name')
+    return;
+  }
+
+  if (validateDirectoryName({branchName: workingDir})){
+    core.setFailed('invalid directory name')
+    return;
+  }
+
+  core.info(`[js-dependency-update]: base branch is ${baseBranch}`);
+  core.info(`[js-dependency-update]: base branch is ${targetBranch}`);
+  core.info(`[js-dependency-update]: base branch is ${workingDir}`);
+
+  awaitexec.exec('npm update', [],{
+    cwd: workingDir
+  });
+
+  const gitStatus = await exec.getExecOutput('git status -s package.json', [],{
+
+  });
+
+  if (gitStatus.stdout.length > 0 ){
+    core.info('[js-dependency-updateThere are upodates available')
+  }else{
+    core.info('[js-dependency-update]: No updates at this point')
+  }
   /*
   1. Parse Inputs:
     1.1 base-branch from which to check for updates
