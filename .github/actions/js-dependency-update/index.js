b98864d9188d1704763eacc30c412d801cb908be
Harshada1999-git <harshada211999neware@gmail.com>
2026-04-22 09:54:15 +0530

    added missing packages

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -4,7 +4,15 @@ async function run() {
   /*
   1. Parse Inputs:
     1.1 base-branch from which to check for updates
-    1.2 target
+    1.2 target-branch to use to create the PR
+    1.3 github Token for authentication purpose (to create PRs)
+    1.4 Working directory for which to check dependencies
+  2. Execute the npm update command within the working directory
+  3. Check whether there are modified package*.json files
+  4. If there are modified files:
+    4.1 Add and Commit files to the target branch 
+    4.2 Create a PR to the base-branch using the Octokit API
+  5. Otherwise, conclude the custom action
   */
   core.info('I am a custom JS action');
 }
